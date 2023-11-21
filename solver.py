3c2537ed0395aa5e231d9b15f72c33d14cc487ed
Marcell Perger <marcell.perger.1@gmail.com>
2023-11-21 21:44:36 +0000

    Add Board methods

--- solver.py
+++ solver.py
@@ -5,7 +5,7 @@ import sys
 from collections import Counter, UserList
 from collections.abc import MutableSequence
 from dataclasses import dataclass
-from typing import IO
+from typing import IO, TypeVar
 
 TUPLE_RANGE9 = TUPLE_0_TO_8 = tuple(range(9))
 SET_1_TO_9 = frozenset(range(1, 9+1))
@@ -31,6 +31,9 @@ def pos_to_idx(pos: tuple[int, int]):
     return pos[0] + pos[1] * 9
 
 
+T = TypeVar('T')
+
+
 @dataclass
 class Board:
     grid: list[int]
@@ -42,6 +45,37 @@ class Board:
             raise ValueError("grid must have 81 elements (9x9)")
         if all(v in range(0, 10) for v in self.grid):
             raise ValueError("Each element of grid must be 0-9")
+
+    @classmethod
+    def nested_to_flat(cls, nested: list[list[T]]) -> list[T]:
+        assert len(nested) == 9 and all(len(v) == 9 for v in nested)
+        return [v for y_list in nested for v in y_list]
+
+    @classmethod
+    def flat_to_nested(cls, flat: list[T]) -> list[list[T]]:
+        assert len(flat) == 9
+        return [[flat[cls.pos_to_idx((x, y))] for y in range(9)] for x in range(9)]
+
+    def copy(self):
+        return Board(self.grid.copy())
+
+    def __getitem__(self, item: tuple[int, int] | int | slice):
+        if isinstance(item, (int, slice)):
+            return self.grid[item]
+        return self.grid[self.pos_to_idx(item)]
+
+    def __setitem__(self, key: tuple[int, int] | int | slice, value: int):
+        if isinstance(key, (int, slice)):
+            self.grid[key] = value
+        self.grid[self.pos_to_idx(key)] = value
+
+    @classmethod
+    def idx_to_pos(cls, idx: int) -> tuple[int, int]:
+        return idx % 9, idx // 9
+
+    @classmethod
+    def pos_to_idx(cls, pos: tuple[int, int]):
+        return pos[0] + pos[1] * 9
 
 
 class InvalidSudokuError(ValueError):
